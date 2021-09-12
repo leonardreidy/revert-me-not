40aa51d8c8d6db9d2b00cade8b608e69dacb002c
Leonard M Reidy <leonard.reidy@gmail.com>
2021-09-12 21:51:33 +0100

    Time = t1

--- feature/index.ts
+++ feature/index.ts
@@ -1,18 +1,22 @@
+// [A]
 const doTheFirstThing = (params) => {
   // do the first thing
   return undefined;
 }
 
+// [A]
 const doTheSecondThing = (params) => {
   // do the second thing
   return undefined;
 }
 
+// [A]
 const doTheThirdThing = (params) => {
   // do the third thing
   return undefined;
 }
 
+// [A]
 const doTheFourthThing = (params) => {
   // do the fourth thing
   return undefined;
